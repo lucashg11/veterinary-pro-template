d185d2d0f40d5184d6f99c2aa5fc4ef627de59f6
Lucas Gomez <gomezlucashernan@hotmail.com>
2026-01-16 16:53:02 -0300

    fix:  Rename to config.ts fixing images problems

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,14 @@
 import { defineCollection } from "astro:content";
 import { z } from "astro/zod";
-const services = defineCollection({
+const servicesCollection = defineCollection({
   schema: ({ image }) =>
     z.object({
       title: z.string(),
       description: z.string(),
       order: z.number(),
       image: image(),
+      imageAlt: z.string(),
     }),
 });
 
-export const collections = { services };
+export const collections = { services: servicesCollection };
